4c63c9185e6a9d2d9e7df5e0d653dd4c5dd4f56b
notglebatall <161127821+notglebatall@users.noreply.github.com>
2024-09-07 12:40:53 +0300

    k

--- main.py
+++ main.py
@@ -27,22 +27,43 @@ async def check_conditions_and_notify(match_list, bot):
         # Уникальный идентификатор события используется, чтобы избежать повторных уведомлений
         if match['name'] not in notified_events:
             logging.info(f"Проверка условия для события {match['name']}")
-            if int(match['time'].split(':')[0]) >= 30 and (int(match['score_1']) == 0 or int(match['score_2']) == 0):
-                message = (f"Матч: {match['team_1']} - {match['team_2']}\n\n"
-                           f"Счет: {match['score_1']} - {match['score_2']}\n"
-                           f"Время игры: {match['time']}\n\n"
-                           f"Коэффициент на {match['team_1']} {match['total_text_1']}:\n\n"
-                           f"Больше: {match['value_more_1']}\n"
-                           f"Меньше: {match['value_less_1']}\n\n"
-                           f"Коэффициент на {match['team_2']} {match['total_text_2']}:\n\n"
-                           f"Больше: {match['value_more_2']}\n"
-                           f"Меньше: {match['value_less_2']}\n\n"
-                           f"Ссылка на событие: {match['link']}")
-                logging.info(f"Отправка уведомления для события {match['name']}")
-                await bot.send_message(chat_id=CHAT_ID, text=message)
-                notified_events.add(match['name'])  # Добавление события в множество уведомленных
+
+            # Основное условие по времени матча
+            if int(match['time'].split(':')[0]) >= 30:
+                # Формируем отдельные части сообщения в зависимости от условий
+                team_1_odds = ""
+                team_2_odds = ""
+
+                # Проверка для первой команды
+                if int(match['score_1']) == 0 and float(match['value_more_1']) > 1.5:
+                    team_1_odds = (f"Коэффициент на {match['team_1']} {match['total_text_1']}:\n\n"
+                                   f"Больше: {match['value_more_1']}\n"
+                                   f"Меньше: {match['value_less_1']}\n\n")
+
+                # Проверка для второй команды
+                if int(match['score_2']) == 0 and float(match['value_more_2']) > 1.5:
+                    team_2_odds = (f"Коэффициент на {match['team_2']} {match['total_text_2']}:\n\n"
+                                   f"Больше: {match['value_more_2']}\n"
+                                   f"Меньше: {match['value_less_2']}\n\n")
+
+                # Если хотя бы одно из дополнительных условий выполнено, формируем сообщение
+                message = ""  # Инициализация переменной message
+                if team_1_odds or team_2_odds:
+                    message = (f"Матч: {match['team_1']} - {match['team_2']}\n\n"
+                               f"Счет: {match['score_1']} - {match['score_2']}\n"
+                               f"Время игры: {match['time']}\n\n"
+                               f"{team_1_odds}"
+                               f"{team_2_odds}"
+                               f"Ссылка на событие: {match['link']}")
+
+                if message:  # Отправляем сообщение только если оно не пустое
+                    logging.info(f"Отправка уведомления для события {match['name']}")
+                    await bot.send_message(chat_id=CHAT_ID, text=message)
+                    notified_events.add(match['name'])  # Добавление события в множество уведомленных
+                else:
+                    logging.info(f"Событие {match['name']} не проходит дополнительные проверки")
             else:
-                print(f'Событие {match['name']} не подходит')
+                logging.info(f"Событие {match['name']} не подходит по времени")
 
 
 async def job():
